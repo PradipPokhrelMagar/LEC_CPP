faf357be71fd8cc29cf92983d5f89c5c22777f12
Pradip Pokhrel Magar <ppokhrelmagar@gmil.com>
2023-08-19 16:05:58 +0545

    Catching all exceptions

--- CLASS_CONTENT/EXCEPTION_HANDLING/allexception.cpp
+++ CLASS_CONTENT/EXCEPTION_HANDLING/allexception.cpp
@@ -1,43 +1,42 @@
+//There is a special catch block called ‘catch all’ catch(…) that can be used to catch all types of exceptions. 
+//For example, in the following program, an int is thrown as an exception, 
+//but there is no catch block for int, so catch(…) block will be executed.
+
 #include <iostream>
 using namespace std;
+// const int size = 10;
 
-const int size=10;
-class Array
-{
+class Array {
     private:
-    int arr[size];
-
-    public:
-    class LOWRANGE{};
-    class HIGHRANGE{};
+        int arr[10];
     
-    int & operator[](int i)
-    {
-        if(i<0)
-        throw LOWRANGE();
-        else if(i>=size)
-        throw HIGHRANGE();
-        return arr[i];
-    }
-};
+    public:
+        class LOWRANGE{};
+        class HIGHRANGE{};
 
-int main()
-{
+        int &operator [] (int i) {
+            if(i < 0) {
+                throw LOWRANGE();
+            }
+            else if( i>=10) {
+                throw HIGHRANGE();
+            }
+            return arr[i];
+        }
+        };
+int main() {
     Array a1;
     int index;
-    try
-    {
-        cout<<"ënter array index:";
+
+    try {
+        cout<<"Enter array index: ";
         cin>>index;
-        a1[index]=10;
-        cout<<index<<"is within range"<<endl;
-
+        a1[index];
+        cout<<"index"<<index <<"is within the range";
+    } 
+    
+    catch(...) {
+        cout<<"Exception out of range";
     }
-    catch(...)
-    {
-        cout<<"exception: out of range";
-
-    }
-
     return 0;
 }
