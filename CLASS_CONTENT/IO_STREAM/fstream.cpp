638169a4802fccabaa1a110891ed328e4d739509
Pradip Pokhrel Magar <ppokhrelmagar@gmil.com>
2023-08-19 15:27:16 +0545

    IO_Stream programs

---
+++ CLASS_CONTENT/IO_STREAM/fstream.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+    fstream file;
+
+    file.open("createfile.txt",ios::out);
+
+    if(!file)
+    {
+        cout<<"Error in creating file!!";
+        return 0;
+
+
+    }
+
+    cout<<"File created successfully";
+
+    file.close();
+    return 0;
+}
---
+++ CLASS_CONTENT/IO_STREAM/using_ios_member.cpp
@@ -0,0 +1,10 @@
+void IOS_fill()
+{
+    cout<<"\n...............\n";
+    cout<<"Implementing ios::fill\n\n";
+    cout ch ='a';
+
+    cout.fill('+');
+    cout.width(10);
+    
+} 
